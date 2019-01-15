3a944e06282db7d9d220da60c92ab6842612974e
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-01-15 23:35:41 +0100

    finalised the Record Classes

--- src/Logic/Records/PlayListRecord.java
+++ src/Logic/Records/PlayListRecord.java
@@ -1,4 +1,44 @@
 package Logic.Records;
 
+import java.util.HashSet;
+
 public class PlayListRecord {
+
+    private int playListID;
+    private String name;
+    private HashSet<SongRecord> songRecords;
+
+    public PlayListRecord(int playListID, String name) {
+        this.playListID = playListID;
+        this.name = name;
+        this.songRecords = new HashSet<>();
+    }
+
+    public void addSongRecord(SongRecord songRecord){
+        this.songRecords.add(songRecord);
+    }
+
+    public int getPlayListID() {
+        return playListID;
+    }
+
+    public void setPlayListID(int playListID) {
+        this.playListID = playListID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HashSet<SongRecord> getSongRecords() {
+        return songRecords;
+    }
+
+    public void setSongRecords(HashSet<SongRecord> songRecords) {
+        this.songRecords = songRecords;
+    }
 }
--- src/Logic/Records/SongRecord.java
+++ src/Logic/Records/SongRecord.java
@@ -1,5 +1,5 @@
 package Logic.Records;
-import java.util.Set;
+import java.util.HashSet;
 
 public class SongRecord {
 
@@ -7,17 +7,85 @@ public class SongRecord {
     private Integer length;
     private Integer nrOfListens;
     private String songName;
-    private Set<ArtistRecord> artistRecords;
-    private Set<AlbumRecord> albumRecords;
-    private Set<String> genres;
+    private HashSet<ArtistRecord> artistRecords;
+    private HashSet<AlbumRecord> albumRecords;
+    private HashSet<String> genres;
 
-    public SongRecord(int songID, Integer length, Integer nrOfListens, String songName, Set<ArtistRecord> artistRecords, Set<AlbumRecord> albumRecords, Set<String> genres) {
+    public SongRecord(int songID, Integer length, Integer nrOfListens, String songName) {
         this.songID = songID;
         this.length = length;
         this.nrOfListens = nrOfListens;
         this.songName = songName;
+        this.artistRecords = new HashSet<>();
+        this.albumRecords = new HashSet<>();
+        this.genres = new HashSet<>();
+    }
+
+    public void addArtistRecord(ArtistRecord artistRecord){
+        this.artistRecords.add(artistRecord);
+    }
+
+    public void addAlbumRecord(AlbumRecord albumRecord){
+        this.albumRecords.add(albumRecord);
+    }
+
+    public void addGenre(String genre){
+        this.genres.add(genre);
+    }
+
+    public int getSongID() {
+        return songID;
+    }
+
+    public void setSongID(int songID) {
+        this.songID = songID;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getNrOfListens() {
+        return nrOfListens;
+    }
+
+    public void setNrOfListens(Integer nrOfListens) {
+        this.nrOfListens = nrOfListens;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public HashSet<ArtistRecord> getArtistRecords() {
+        return artistRecords;
+    }
+
+    public void setArtistRecords(HashSet<ArtistRecord> artistRecords) {
         this.artistRecords = artistRecords;
+    }
+
+    public HashSet<AlbumRecord> getAlbumRecords() {
+        return albumRecords;
+    }
+
+    public void setAlbumRecords(HashSet<AlbumRecord> albumRecords) {
         this.albumRecords = albumRecords;
+    }
+
+    public HashSet<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(HashSet<String> genres) {
         this.genres = genres;
     }
 }
