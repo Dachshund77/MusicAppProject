b33b8198cf0606909e1fdeb6804cf13f5575c9c6
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-01-16 01:07:36 +0100

    Fixed ViewController

--- src/Logic/Player/ViewController.java
+++ src/Logic/Player/ViewController.java
@@ -13,16 +13,16 @@ import java.io.IOException;
  * The enum class will store the relative path to the fxml so that we don't need to type it multiple times.
  */
 public enum ViewController {
-    MAIN("../Fxmls/Main.fxml"),
-    MUSIC_PLAYER("../Fxmls/MusicPlayer.fxml"),
-    PLAYLIST("../Fxmls/PlayList.fxml"),
-    SONGLIST("../Fxmls/SongList.fxml"),
-    UPDATE_ALBUM("../Fxmls/UpdateAlbum.fxml"),
-    UPDATE_ARTIST("../Fxmls/UpdateArtist.fxml"),
-    UPDATE_GENRE("../Fxmls/UpdateGenre.fxml"),
-    UPDATE_PLAYLIST("../Fxmls/UpdatePlayList.fxml"),
-    UPDATE_SONG("../Fxmls/UpdateSong.fxml"),
-    ADD_SONG_TO_PLAYLIST("../Fxmls/AddSongToPlayList.fxml");
+    MAIN("../../Fxmls/Main.fxml"),
+    MUSIC_PLAYER("../../Fxmls/MusicPlayer.fxml"),
+    PLAYLIST("../../Fxmls/PlayList.fxml"),
+    SONGLIST("../../Fxmls/SongList.fxml"),
+    UPDATE_ALBUM("../../Fxmls/UpdateAlbum.fxml"),
+    UPDATE_ARTIST("../../Fxmls/UpdateArtist.fxml"),
+    UPDATE_GENRE("../../Fxmls/UpdateGenre.fxml"),
+    UPDATE_PLAYLIST("../../Fxmls/UpdatePlayList.fxml"),
+    UPDATE_SONG("../../Fxmls/UpdateSong.fxml"),
+    ADD_SONG_TO_PLAYLIST("../../Fxmls/AddSongToPlayList.fxml");
 
     private final String URL;
 
